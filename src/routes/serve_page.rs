bb1343fce86e9f6058534c9671c839b4963d4e9e
Bodleum <daniel@daniellaing.com>
2023-04-14 17:54:47 +0100

    Better error on serve_page

--- src/routes/serve_page.rs
+++ src/routes/serve_page.rs
@@ -1,11 +1,22 @@
 use std::path::Path;
 
 use axum::response::{Html, IntoResponse, Response};
+use hyper::StatusCode;
 
 pub async fn serve_page<P>(path: &P) -> Response
 where
     P: AsRef<Path>,
 {
-    let content = tokio::fs::read_to_string(path).await.unwrap();
+    let content = match tokio::fs::read_to_string(path).await {
+        Ok(ok) => ok,
+        Err(err) => {
+            return (
+                StatusCode::NOT_FOUND,
+                format!("Error reading {}. {}.", path.as_ref().display(), err),
+            )
+                .into_response()
+        }
+    };
+
     Html(content).into_response()
 }

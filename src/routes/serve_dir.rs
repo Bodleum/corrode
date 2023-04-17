fff0ed47870de7d630931c7018f58c9809c33089
Bodleum <daniel@daniellaing.com>
2023-04-17 16:31:08 +0100

    Filtering relevent files

--- src/routes/serve_dir.rs
+++ src/routes/serve_dir.rs
@@ -15,7 +15,11 @@ where
 {
     let fs_path = state.content_root.join(path);
 
-    let foo = contents(&fs_path).await?;
+    let mut foo = Vec::new();
+    for f in rel_contens(&fs_path).await? {
+        foo.push(f);
+    }
+
     let placeholder_message = format!(
         "The method to show this page has not been implemented yet! {}",
         fs_path.display()
@@ -24,14 +28,17 @@ where
     Ok((StatusCode::NOT_IMPLEMENTED, placeholder_message).into_response())
 }
 
-async fn contents<P>(path: &P) -> io::Result<Vec<File>>
+async fn rel_contens<P>(path: &P) -> io::Result<Vec<File>>
 where
     P: AsRef<Path>,
 {
     let mut ret = Vec::new();
     let mut reader = fs::read_dir(path).await?;
     while let Some(entry) = reader.next_entry().await? {
-        ret.push(File::open(entry.path()).await?);
+        // Want only directories or markdown files
+        if (entry.path().is_dir()) || (entry.path().extension().unwrap_or_default() == "md") {
+            ret.push(File::open(entry.path()).await?);
+        }
     }
 
     Ok(ret)
